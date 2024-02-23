5e5a689055d91c65eebd5d5ceaa68a89259c835d
Henry <henryli12138@gmail.com>
2024-02-22 20:17:59 -0700

    Add existing file

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/hero.jsx"
+import Card from "./components/Card"
+import data from "./data.js"
+
+export default function App() {
+  const cards = data.map(cardData => {
+    return (
+      <Card
+        img={cardData.coverImg}
+        rating={cardData.stats.rating}
+        reviewCount={cardData.stats.reviewCount}
+        location={cardData.location}
+        title={cardData.title}
+        price={cardData.price}
+      />
+    )
+  })
+  
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+        {cards}
+      </section>
+    </div>
+  )
+}
